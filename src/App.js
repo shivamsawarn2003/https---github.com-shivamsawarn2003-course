c2230df2b90b1437ca62b909139cb1f21ce0448a
shivamkvs <shivamsawarn2003@gmail.com>
2024-08-21 11:12:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import styled,{ThemeProvider} from "styled-components";
-import {lightTheme} from "./utils/Themes";
+import styld,{ThemeProvider} from "styled-components";
+import {ligtTheme} from "./utils/Themes";
 import {BrowserRouter,Routes,Route} from "react-router-dom";
 
-const Container = styled.div`
+const Contaier = styld.div`
 width:100%;
 height:100vh;
 display:flex;
@@ -10,7 +10,7 @@ flex-direction:column;
 `;
 
 function App() {
-return <ThemeProvider theme={lightTheme}>
+return <ThemeProvider theme={ligtTheme}>
   <BrowserRouter>
 </BrowserRouter>
 </ThemeProvider>
